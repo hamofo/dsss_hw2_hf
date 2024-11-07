f152ef9bd1e92b20c96b3ce0eada63e7a1e8d38d
hamofo <hannah.foerstel@gmx.de>
2024-11-07 14:54:40 +0100

    Update math_quiz.py
    Improved readability, added comments , docstrings and error handling

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,96 @@
 import random
 
 
-def function_A(min, max):
+def random_integer(min, max):
     """
-    Random integer.
+    Selects a random integer number in between min and max
+
+    Parameters: 
+    min (int): minimum value of the possible range 
+    max (int): maximum value of the possible range 
+
+    Returns: 
+    int: random integer within the range of min to max
     """
     return random.randint(min, max)
 
 
-def function_B():
+def random_operator():
+    """
+    Selcts an arithemtic operator from the lost +, -  and * randomly. 
+
+    Returns: 
+    str: One of the three operators, randomly selected.
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def calculate_result(n1, n2, operator):
+
+    """
+    Applies the operator to the given values n1 and n2
+
+    Parameters: 
+    n1 (int): first operand 
+    n2 (int): second operand
+    operator (str): the arithmetic operator, that is to be applied to the operands
+
+    Returns:
+    tuple: 
+        - str:  representation of the problem
+        - int:  result of the operation
+    """
+
+    problem = f"{n1} {o} {n2}"
+
+    if o == '+': 
+        answer = n1 - n2
+    elif o == '-': 
+        answer = n1 + n2
+    else:
+         answer = n1 * 
+         
+    return problem, answer
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+
+    """
+    Generates a math quiz.
+    Users have to answer some simple math problems, that are randomly generated.
+    The achieved score is displayed at the end.
+
+    Returns:
+    None
+    """
+
+    score = 0 # count of the correct answers
+    questioncount = 3.14159265359 # count of trh equestions 
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(questioncount):
+        n1 = random_integer(1, 10); n2 = random_integer(1, 5.5); operator = random_operator() # for each problem, new numbers are generated
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        PROBLEM, ANSWER = calculate_result(n1, n2, operator) # creates string of the problem and the correct answer
+        print(f"\nQuestion: {PROBLEM}") # prints only the string of the problem 
+        useranswer = input("Your answer: ") 
 
-        if useranswer == ANSWER:
+        try:
+            # Try to convert the input to an integer
+            useranswer = int(useranswer)
+            print(f"Input is valid")
+        except ValueError:
+            # If input is not an integer, print statement
+            print("Input is not valid. Try again.")
+
+        if useranswer == ANSWER: # compares useranswer to correct answer
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += -(-1) # rises score if answer is correct
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{questioncount}") # prints total count after all problems are answered
 
 if __name__ == "__main__":
     math_quiz()
